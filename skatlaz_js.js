e7cb6f64c59bf340d9cbdb37e9e0ddb897c9ce5d
smool <igorfb4@hotmail.com>
2026-01-20 03:02:36 -0300

    Update skatlaz_js.js

--- skatlaz_js.js
+++ skatlaz_js.js
@@ -9,16 +9,24 @@ function _set_addr(_path){
 function _skatlaz_remote(){
     _r = 'false'
     fetch(_addr_json)
-        .then(response => response.json();_data_json = response;)
-        .then(_r = data => console.log(data);_data_json = data;)
-        .catch(_r = error => console.error('Error fetching data:', error));
-    _r = response;
-    window.alert(response);
-    return _r
+        .then(response => {
+            if(response.ok){
+                _data_json = response.json();
+                return response.json(); 
+            }
+        })
+        .then(data => {
+                console.log(data);
+                _data_json = data;
+                return data;
+        })
+        .catch(error => {
+                return error;
+        });
+        return _r;
 }
 
 function bind(dom_obj){
-        window.alert(dom_obj);
         const container = dom_obj;
         _mvap = [];
         let _data = Object.keys(_data_json);
@@ -28,20 +36,20 @@ function bind(dom_obj){
             _vmv = {"field":_vmv_tx0,"val":_vmv_tx1};
             _data_dict.push(_vmv);
         });
-        let _dom = Object.keys(dom_obj);
-        window.alert(_dom);
-        _dom.forEach(ddx => {
-            c = 0;
-            if(ddx.TextContent.indexof('{' + _data_dict[c].field + '}')){
-                _ix = ddx.TextContent.indexof('{' + _data_dict[c].field + '}');
-                _ixfl = '{' + mvap[c].field + '}';
-                _ixl = _ixfl.length;
-                _six = ddx.TextContent.substring(_ix,_ixl);
-                _mv_0r = _six.replace(_six, _data_dict[c].val).replace('{','').replace('}','');
-                ddomx = dxx.replace(_six,_mv_0r);
-                container.appendChild(ddomx);
-                window.alert(ddomx);
-            }
-            ++c;
+        _data_dict.forEach(obj_x => {
+            let _dom = Object.keys(dom_obj);
+            _dom.forEach(ddx => {
+                c = 0;
+                if(ddx.TextContent.indexof('{' + _obj_x.field + '}')){
+                    _ix = ddx.TextContent.indexof('{' + _obj_x.field + '}');
+                    _ixfl = '{' +_obj_x.field + '}';
+                    _ixl = _ixfl.length;
+                    _six = ddx.TextContent.substring(_ix,_ixl);
+                    _mv_0r = _six.replace(_six, _obj_x.val).replace('{','').replace('}','');
+                    ddomx = dxx.replace(_six,_mv_0r);
+                    container.appendChild(ddomx);
+                }
+                c++;
+            });
         });
 }
